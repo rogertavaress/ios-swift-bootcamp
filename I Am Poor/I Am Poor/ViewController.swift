a15cfc1ebd2533bbb481b7bd9384f42cb916e8de
Rogério Tavares <rogerioctf@gmail.com>
2021-07-24 13:58:15 -0300

    Criando novo projeto no xcode

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Rogério Tavares on 24/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
