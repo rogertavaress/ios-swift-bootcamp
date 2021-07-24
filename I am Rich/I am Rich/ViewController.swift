f4b4af0c4bafe04b68dba84b1b23628e1f643f82
Rogério Tavares <rogerioctf@gmail.com>
2021-07-24 13:51:25 -0300

    first commit - i am rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Rogério Tavares on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
