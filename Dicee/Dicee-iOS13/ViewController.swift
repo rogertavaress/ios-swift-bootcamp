61cdf1bc5ce972e01a396ed420ef7ad8629c06c5
Rogério Tavares <rogerioctf@gmail.com>
2021-07-25 14:07:13 -0300

    components - dicee

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Componentes de imagem - dados
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView2.alpha = 0.5
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped")
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView1.alpha = 1
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.alpha = 1
+    }
+    
 }
 
