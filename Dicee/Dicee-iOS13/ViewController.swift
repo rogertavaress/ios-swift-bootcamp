9462468ad187f80f1f8f97addf7ef1a38505692b
Rogério Tavares <rogerioctf@gmail.com>
2021-07-26 21:00:23 -0300

    variaveis e random

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -14,25 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let options = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
+        diceImageView1.image = options.randomElement()
+        diceImageView2.image = options.randomElement()
         
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView1.alpha = 0.5
         diceImageView2.alpha = 0.5
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped")
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
         diceImageView1.alpha = 1
-        
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
         diceImageView2.alpha = 1
+        
+        diceImageView1.image = options.randomElement()
+        diceImageView2.image = options.randomElement()
     }
     
 }
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    //The number of letters in alphabet equals 26
+    
+let password = "\(alphabet.randomElement())\(alphabet.randomElement())\(alphabet.randomElement())\(alphabet.randomElement())\(alphabet.randomElement())\(alphabet.randomElement())"
+    
+print(password)
+    
